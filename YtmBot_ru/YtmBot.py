47fb2ddce1740f7e982b2cdff83db93720ff6de5
Vladyslav Vodopianov <vodvlad2001@gmail.com>
2022-07-11 14:06:43 +0200

    Update YtmBot.py

--- YtmBot_ru/YtmBot.py
+++ YtmBot_ru/YtmBot.py
@@ -8,7 +8,7 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, '👋 Hi, <b>' + message.from_user.first_name + '</b>!\nWelcome to YouTube Music Downloader.\nSend me a YouTube <b>video/music</b> link or an <b>.mp3</b>🎵 file.', parse_mode="html")
+    bot.send_message(message.chat.id, '👋 Привет, <b>' + message.from_user.first_name + '</b>!\nДобро пожаловать в загрузчик YouTube Music.\nОтправь мне ссылку на YouTube <b>видео/музыку</b> или файл формата <b>.mp3</b>🎵.', parse_mode="html")
 
 @bot.message_handler(content_types=['audio'])
 def handle_docs_photo(message):
@@ -21,9 +21,9 @@ def handle_docs_photo(message):
         src = message.audio.file_name
         with open(src, 'wb') as new_file:
             new_file.write(downloaded_file)
-        bot.reply_to(message, "⏳Starting loading...")
+        bot.reply_to(message, "⏳Начинаю загрузку...")
         upload(src)
-        bot.reply_to(message, "✅ Downloaded, enjoy!")
+        bot.reply_to(message, "✅ Загрузил, наслаждайся!")
         
 
     except Exception as e:
@@ -32,10 +32,10 @@ def handle_docs_photo(message):
 @bot.message_handler(content_types=['text'])
 def sendmessage(message):
     if(message.text[0] == 'h' and message.text[1] == 't' and message.text[2] == 't'):
-        bot.reply_to(message, "⏳Starting loading...")
+        bot.reply_to(message, "⏳Начинаю загрузку...")
         zxc = get_mp3(message.text)
         upload(zxc)
-        bot.reply_to(message, "✅ Downloaded, enjoy!")
+        bot.reply_to(message, "✅ Загрузил, наслаждайся!")
 
 
 bot.polling(none_stop=True)
